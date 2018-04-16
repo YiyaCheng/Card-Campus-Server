6b816b901befc861afcf593d3795e0a6e9f1dd68
YiyaCheng <cyiyacheng@163.com>
2018-04-16 15:26:39 +0800

    pojo

---
+++ src/pojo/QuestionReply.java
@@ -0,0 +1,42 @@
+package pojo;
+
+import java.sql.Timestamp;
+
+public class QuestionReply {
+	int breply_id;
+	QuestionPost questionPost;
+	User user;
+	String breply_content;
+	Timestamp breply_time;
+	public int getBreply_id() {
+		return breply_id;
+	}
+	public void setBreply_id(int breply_id) {
+		this.breply_id = breply_id;
+	}
+	public QuestionPost getQuestionPost() {
+		return questionPost;
+	}
+	public void setQuestionPost(QuestionPost questionPost) {
+		this.questionPost = questionPost;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getBreply_content() {
+		return breply_content;
+	}
+	public void setBreply_content(String breply_content) {
+		this.breply_content = breply_content;
+	}
+	public Timestamp getBreply_time() {
+		return breply_time;
+	}
+	public void setBreply_time(Timestamp breply_time) {
+		this.breply_time = breply_time;
+	}
+	
+}
--- src/pojo/User.java
+++ src/pojo/User.java
@@ -1,5 +1,47 @@
 package pojo;
 
 public class User {
-
+	int user_sno;
+	String user;
+	String user_avatar;
+	String user_sex;
+	int user_tel;
+	int user_qq;
+	public int getUser_sno() {
+		return user_sno;
+	}
+	public void setUser_sno(int user_sno) {
+		this.user_sno = user_sno;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getUser_avatar() {
+		return user_avatar;
+	}
+	public void setUser_avatar(String user_avatar) {
+		this.user_avatar = user_avatar;
+	}
+	public String getUser_sex() {
+		return user_sex;
+	}
+	public void setUser_sex(String user_sex) {
+		this.user_sex = user_sex;
+	}
+	public int getUser_tel() {
+		return user_tel;
+	}
+	public void setUser_tel(int user_tel) {
+		this.user_tel = user_tel;
+	}
+	public int getUser_qq() {
+		return user_qq;
+	}
+	public void setUser_qq(int user_qq) {
+		this.user_qq = user_qq;
+	}
+	
 }
