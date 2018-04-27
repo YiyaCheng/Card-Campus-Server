b16223df1430306d4d77c9b2ba424ce32e6b9875
hishark <hishark@yeah.net>
2018-04-27 17:09:31 +0800

    实现客户端对服务器的访问。

--- src/control/DaiPostController.java
+++ src/control/DaiPostController.java
@@ -1,14 +1,20 @@
 package control;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import pojo.DaiPost;
-import pojo.LovePost;
 import service.DaiPostService;
 
 @Controller
@@ -19,6 +25,10 @@ public class DaiPostController {
 	
 	/**
 	 * 1.代课的相关操作
+	 */
+	
+	/**
+	 * 服务器
 	 */
 	@RequestMapping("listDaike")//相当于struts.xml中的action的名字name
 	public ModelAndView ListDaike(){
@@ -44,6 +54,58 @@ public class DaiPostController {
 		mav.setViewName("index");
 		return mav;
 	}
+	
+	
+	/**
+	 * 客户端
+	 */
+	//处理来自客户端的请求，并将json格式的数据处理结果返回。
+    @RequestMapping("getDaikeList")
+    public void getUserFromClient(HttpServletRequest request, HttpServletResponse response){
+        JSONObject jsonObject = new JSONObject();
+        List<DaiPost> alldaike=daiPostService.daikeList();
+        JSONArray arrayDaike = JSONArray.fromObject(alldaike);
+        /*
+		 * 先封装成text即字符串 再转换成JSON
+		 * 安卓Activity的中文显示只认UTF-8！GBK不可以哦
+		 * 这里是指在客户端显示的编码格式
+		 */
+		response.setContentType("text/json;charset=utf-8");
+		
+		/*
+		 * 这里是指在网络传输过程中的编码方式
+		 */
+		response.setCharacterEncoding("utf-8");
+		
+		/*
+		 * 用管道流传东西啦
+		 * 把JSON转换成byte再传
+		 * 编码方式UTF-8！
+		 */
+		try {
+			byte[] bytes = arrayDaike.toString().getBytes("utf-8");
+			
+			//把字节数组写入输出流
+			response.getOutputStream().write(bytes);
+			
+			//设置传输内容的长度，方便response处理
+			response.setContentLength(bytes.length);
+			
+			//清空缓存（把缓存里的全部发出去
+			response.getOutputStream().flush();
+			
+			//用完要关啦
+			response.getOutputStream().close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
+
+    }
+	
+	
+	
 	
 	/**
 	 * 2.代拿外卖的相关操作
